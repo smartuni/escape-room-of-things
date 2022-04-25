afb87f4782b2d0717c00d365447406624b50f11f
Leandro Lanzieri <leandro.lanzieri@haw-hamburg.de>
2022-04-25 16:02:46 +0200

    Nodes: add example of external module and board

---
+++ Nodes/example/main.c
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2022 HAW Hamburg
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @{
+ *
+ * @file
+ * @brief       Just an example application
+ *
+ * @author      Leandro Lanzieri <leandro.lanzieri@haw-hamburg.de>
+ *
+ * @}
+ */
+
+#include <stdio.h>
+#include "example_module.h"
+
+int main(void)
+{
+    puts(external_module_message);
+    return 0;
+}
