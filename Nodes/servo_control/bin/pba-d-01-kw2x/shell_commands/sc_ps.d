5fdcada3b69f7edf257102e53f03d99e222045f1
TristanImkn <tristan.imken@haw-hamburg.de>
2022-05-09 14:23:49 +0200

    Removed bin files

--- Nodes/servo_control/bin/pba-d-01-kw2x/shell_commands/sc_ps.d
+++
@@ -1,8 +0,0 @@
-/mnt/fileserver/MyHome/backup/bin/pba-d-01-kw2x/shell_commands/sc_ps.o: \
- /mnt/fileserver/MyHome/escape-room-of-things/Nodes/RIOT/sys/shell/commands/sc_ps.c \
- /mnt/fileserver/MyHome/backup/bin/pba-d-01-kw2x/riotbuild/riotbuild.h \
- /mnt/fileserver/MyHome/escape-room-of-things/Nodes/RIOT/sys/include/ps.h
-
-/mnt/fileserver/MyHome/backup/bin/pba-d-01-kw2x/riotbuild/riotbuild.h:
-
-/mnt/fileserver/MyHome/escape-room-of-things/Nodes/RIOT/sys/include/ps.h:
