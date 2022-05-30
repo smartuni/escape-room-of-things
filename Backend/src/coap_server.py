1ad4b6d58a3c85ed85667775548e27353c7bde52
act958 <Fridolinski4>
2022-05-30 13:06:34 +0200

    observe_device changes

--- Backend/src/coap_server.py
+++ Backend/src/coap_server.py
@@ -1,14 +1,16 @@
 import asyncio
 import configparser
 import os
-import aiocoap.resource as resource
 import re
+
+import aiocoap.resource as resource
 from aiocoap import *
 from flask import Flask
-from orm_classes.shared import db
+
 from orm_classes.Device import Device
 from orm_classes.Puzzle import Puzzle
 from orm_classes.Room import Room
+from orm_classes.shared import db
 
 config = configparser.ConfigParser()
 config.read(os.path.join(os.path.dirname(__file__), 'restconfig.ini'))
@@ -45,7 +47,6 @@ async def victory(room, con):
 
 
 async def check_game_state(device, con):
-    set_device_solved(device)  # For Tests
     puzzle_id = device.puzzle
     puzzle = Puzzle.query.filter_by(id=puzzle_id).first()
     if check_puzzle_state(puzzle):
@@ -75,7 +76,6 @@ def check_puzzle_state(puzzle):
     return True
 
 
-# For Tests
 def set_device_solved(device):
     device.state = "solved"
     db.session.commit()
@@ -93,7 +93,7 @@ async def add_new_devices(devices, ips, con):
                     db.session.add(d)
                     db.session.commit()
                     ips.append(matches.group(2))
-                    await observe_device(d,con)
+                    await observe_device(d, con)
     return ips
 
 
@@ -103,11 +103,10 @@ def get_devices_from_db():
         return [d.devIP for d in devs]
 
 
-async def observe_device(device,con):
-    con = await Context.create_client_context()
+async def observe_device(device, con):
+    print(device.name)
     request = Message(code=GET, uri="coap://{}/node/info".format(device.devIP),
-                    observe=0)
-
+                      observe=0)
     req = con.request(request)
     res = await req.response
     print(res)
@@ -115,21 +114,22 @@ async def observe_device(device,con):
 
     print("observe: {}".format(device.devIP))
     async for r in req.observation:
-        device.state = "solved"
+        # device.state = r.response.payload.state
+        # db.session.commit()
         print(r)
         print(r.payload)
         # parse answer, CBOR?
         # implement if
-        #check_game_state(device)
+        # check_game_state(device)
 
 
 async def main():
     connectedIps = []
-    connectedIps = get_devices_from_db()
+    # connectedIps = get_devices_from_db()
     root = resource.Site()
 
     root.add_resource(['.well-known', 'core'],
-                    resource.WKCResource(root.get_resources_as_linkheader))
+                      resource.WKCResource(root.get_resources_as_linkheader))
     root.add_resource(['whoami'], WhoAmI())
 
     con = await Context.create_server_context(root, bind=("127.0.0.1", 5555))
@@ -147,7 +147,6 @@ async def main():
         li = r.payload.decode('utf-8').split(",")
 
         connectedIps = await add_new_devices(li, connectedIps, con)
-        
 
     # Run forever
     print("server running now")
