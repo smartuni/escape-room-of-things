848c453330c7817531c527844c7d875f077acc0d
act958 <jonathan.scherer@haw-hamburg.de>
2022-06-20 13:18:49 +0200

    server-con out of gamelogic into DTLS Comm.
    get_client_con added.

--- Backend/src/coap_server.py
+++ Backend/src/coap_server.py
@@ -3,15 +3,13 @@ import configparser
 import os
 import platform
 import re
-import json
-import cbor2
 
 import aiocoap.resource as resource
+import cbor2
 from aiocoap import *
-from flask import Flask
-from aiocoap.numbers.codes import GET, PUT
 from aiocoap.credentials import CredentialsMap
-
+from aiocoap.numbers.codes import GET, PUT
+from flask import Flask
 
 from orm_classes.Device import Device
 from orm_classes.Puzzle import Puzzle
@@ -46,7 +44,7 @@ class WhoAmI(resource.Resource):
                        payload="\n".join(text).encode('utf8'))
 
 
-class DTLSCredential (CredentialsMap):
+class DTLSCredential(CredentialsMap):
     psk = ""
 
     def credentials_from_request(self, msg):
@@ -54,6 +52,7 @@ class DTLSCredential (CredentialsMap):
         print(msg)
         print(self.psk)
         return self.psk
+
 
 class coap_server:
     async def init(self):
@@ -70,120 +69,120 @@ class coap_server:
                 matches = re.search('ep="(.+?)";base="coap://(.+?)";', dev)
                 with db_app.app_context():
                     d = Device.query.filter_by(serial=matches.group(1)).first()
-                    if d is not None:
+                    if d is not None and matches.group(1) not in self.connectedDevices:
                         d.devIP = matches.group(2)
                         d.node_state = "connected"
                         db.session.commit()
-                        if matches.group(1) not in self.connectedDevices:
-                            self.connectedDevices.append(matches.group(1))
-                            print(matches.group(1) + " connected")
-                            await self.observe_device(d)
+                        self.connectedDevices.append(matches.group(1))
+                        print(matches.group(1) + " connected")
+                        tasks.append(asyncio.create_task(self.observe_device(d)))
 
-    def device_disconnected(self, devices):
-        print('check for divices to disconnect')
-        con_serials = []
-        removed_serials = []
-        for dev in devices:
-            if "ep=" in dev:
-                matches = re.search('ep="(.+?)";', dev)
-                con_serials.append(matches.group(1))
-        print(con_serials)
-        for serial in self.connectedDevices:
-            if serial not in con_serials:
-                with db_app.app_context():
-                    d = Device.query.filter_by(serial=serial).first()
-                    d.node_state = "disconnected"
-                    db.session.commit()
-                removed_serials.append(serial)
-                print(serial + " disconnected")
-        self.connectedDevices = [
-            x for x in self.connectedDevices if x not in removed_serials]
 
-    def get_devices_from_db(self):
-        with db_app.app_context():
-            devs = Device.query.all()
-        self.connectedDevices = [d.serial for d in devs]
+def device_disconnected(self, devices):
+    print('check for divices to disconnect')
+    con_serials = []
+    removed_serials = []
+    for dev in devices:
+        if "ep=" in dev:
+            matches = re.search('ep="(.+?)";', dev)
+            con_serials.append(matches.group(1))
+    print(con_serials)
+    for serial in self.connectedDevices:
+        if serial not in con_serials:
+            with db_app.app_context():
+                d = Device.query.filter_by(serial=serial).first()
+                d.node_state = "disconnected"
+                db.session.commit()
+            removed_serials.append(serial)
+            print(serial + " disconnected")
+    self.connectedDevices = [
+        x for x in self.connectedDevices if x not in removed_serials]
 
-    async def observe_device(self, device):
-        print('start observe on ' + device.serial)
-        try:
-            dtls = DTLSCredential()
-            dtls.psk = device.psk
-            con= await Context.create_client_context()
-            uri = f"coaps://{device.devIP}:5684/node/info"
-            print(device.psk + ' ' + device.qrid)
-            con.client_credentials.load_from_dict(
-                {uri: {'dtls': {'psk': device.psk.encode(), 'client-identity': device.qrid.encode()}}})
-            request = Message(code=GET, uri=uri, observe=0)
-            req = con.request(request)
-            
-            res = await req.response
-            unpacked = cbor2.loads(res.payload)
+
+def get_devices_from_db(self):
+    with db_app.app_context():
+        devs = Device.query.all()
+    self.connectedDevices = [d.serial for d in devs]
+
+
+async def observe_device(self, device):
+    print('start observe on ' + device.serial)
+    try:
+        con, uri = get_client_con(device, "info")
+        request = Message(code=GET, uri=uri, observe=0)
+        req = con.request(request)
+
+        res = await req.response
+        unpacked = cbor2.loads(res.payload)
+        print(unpacked)
+        device.state = unpacked["puzzleState"]
+        db.session.commit()
+        # trigger cascading logic to check for solved
+        if unpacked["puzzleState"] == "solved":
+            check_game_state(device)
+
+        async for r in req.observation:
+            unpacked = cbor2.loads(r.payload)
             print(unpacked)
             device.state = unpacked["puzzleState"]
             db.session.commit()
             # trigger cascading logic to check for solved
-            #if unpacked["puzzleState"] == "solved":
-            #    check_game_state(device)
-
-            async for r in req.observation:
-                unpacked = cbor2.loads(r.payload)
-                print(unpacked)
-                device.state = unpacked["puzzleState"]
-                db.session.commit()
-                # trigger cascading logic to check for solved
-                #if unpacked["puzzleState"] == "solved":
-                #    check_game_state(device)
-        except Exception as e:
-            print('device observe troubles')
-            print(e)
-
-    async def observe_rd(self):
-        request = Message(code=GET, uri="coap://[::1]:5683/endpoint-lookup/",
-                          observe=0)
-        req = self.con.request(request)
-        res = await req.response
-        cachedli = res.payload.decode('utf-8').split(",")
-        await self.device_connected(cachedli)
-
-        print("start observe loop")
-        async for r in req.observation:
-            li = r.payload.decode('utf-8').split(",")
-            await self.device_connected(li)
-
-    async def poll_rd(self):
-        print("start poll loop")
-        while True:
-            connect_check_request = Message(code=GET, uri="coap://[::1]:5683/endpoint-lookup/",
-                                            observe=1)
-            con_req = self.con.request(connect_check_request)
-            con_res = await con_req.response
-            con_devices = con_res.payload.decode('utf-8').split(",")
-            print(con_devices)
-            self.device_disconnected(con_devices)
-            await self.device_connected(con_devices)
-            await asyncio.sleep(30)
+            if unpacked["puzzleState"] == "solved":
+                check_game_state(device)
+    except Exception as e:
+        print('device observe troubles')
+        print(e)
 
 
-async def victory(room, con):
+async def observe_rd(self):
+    request = Message(code=GET, uri="coap://[::1]:5683/endpoint-lookup/",
+                      observe=0)
+    req = self.con.request(request)
+    res = await req.response
+    cachedli = res.payload.decode('utf-8').split(",")
+    await self.device_connected(cachedli)
+
+    print("start observe loop")
+    async for r in req.observation:
+        li = r.payload.decode('utf-8').split(",")
+        await self.device_connected(li)
+
+
+async def poll_rd(self):
+    print("start poll loop")
+    while True:
+        connect_check_request = Message(code=GET, uri="coap://[::1]:5683/endpoint-lookup/",
+                                        observe=1)
+        con_req = self.con.request(connect_check_request)
+        con_res = await con_req.response
+        con_devices = con_res.payload.decode('utf-8').split(",")
+        print(con_devices)
+        self.device_disconnected(con_devices)
+        await self.device_connected(con_devices)
+        await asyncio.sleep(30)
+
+
+async def victory(room):
     victory_puzzle = next(
         filter(lambda puzzle: puzzle.isVictory.isTrue, room.puzzles))
-    for dev in victory_puzzle:
-        request = Message(code=PUT, uri=f"coap://{dev.devIP}/node/maintenance",
+
+    for device in victory_puzzle:
+        con, uri = get_client_con(device, "maintenance")
+        request = Message(code=PUT, uri=uri,
                           observe=1, payload=None)
         req = con.request(request)
         await req.response
 
 
-async def check_game_state(device, con):
+async def check_game_state(device):
     puzzle_id = device.puzzle
     puzzle = Puzzle.query.filter_by(id=puzzle_id).first()
     if check_puzzle_state(puzzle):
-        await trigger_event(puzzle, con)
+        await trigger_event(puzzle)
         room_id = puzzle.room
         room = Room.query.filter_by(id=room_id).first()
         if check_room_state(room):
-            await victory(room, con)
+            await victory(room)
 
 
 def check_room_state(room):
@@ -211,22 +210,35 @@ def set_device_solved(device):
     db.session.commit()
 
 
-async def trigger_event(puzzle, con):
+async def trigger_event(puzzle):
     event_devices = list(
         filter(lambda device: device.is_event_device, puzzle.devices))
     if len(event_devices) != 0:
         for dev in event_devices:
-            request = Message(code=PUT, uri=f"coap://{dev.devIP}/node/maintenance",
+            con, uri = get_client_con(dev, "maintenance")
+            request = Message(code=PUT, uri=uri,
                               observe=1, payload=None)
             req = con.request(request)
             await req.response
+
+
+def get_client_con(device, path):
+    con = await Context.create_client_context()
+    uri = f"coaps://{device.devIP}:5684/node/{path}"
+    print(device.psk + ' ' + device.qrid)
+    con.client_credentials.load_from_dict(
+        {uri: {'dtls': {'psk': device.psk.encode(), 'client-identity': device.qrid.encode()}}})
+    return con, uri
+
+
+tasks = []
 
 
 async def main():
     server = coap_server()
     await server.init()
 
-    tasks = map(asyncio.create_task, [server.poll_rd()])
+    tasks.append(asyncio.create_task(server.poll_rd()))
     await asyncio.wait(tasks)
 
     print("server running now")
